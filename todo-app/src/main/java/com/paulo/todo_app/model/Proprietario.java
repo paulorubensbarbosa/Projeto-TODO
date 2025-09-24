3f6b84edd2e9e77840a30dffdf26bd9be2858c2d
Paulo Rubens <paulorubensbarbosa@outlook.com>
2025-09-24 00:41:09 -0300

    added: entity Proprietario

--- todo-app/src/main/java/com/paulo/todo_app/model/Proprietario.java
+++ todo-app/src/main/java/com/paulo/todo_app/model/Proprietario.java
@@ -1,10 +1,46 @@
 package com.paulo.todo_app.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "PROPRIETARIO", schema = "TODO_APP")
 public class Proprietario {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private Long id;
+
+    @Column(name = "NOME", nullable = false, length = 500)
+    private String nome;
+
+    @OneToMany(mappedBy = "proprietario", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Tarefa> tarefas = new ArrayList<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public List<Tarefa> getTarefas() {
+        return tarefas;
+    }
+
+    public void setTarefas(List<Tarefa> tarefas) {
+        this.tarefas = tarefas;
+    }
 }
